c7f702282f8c86c78bdaea34423ccee076d700b9
leeseoshim <leeseoit@gmail.com>
2022-08-27 13:27:29 +0900

    백준1주차완료

---
+++ BeakJoon/week1/10926.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync('./input.txt','utf8');
+
+//이미 존재하는 아이디가 주어졌을 때 ??!
+console.log(`${input}??!`);
--- BeakJoon/week1/10998.js
+++ BeakJoon/week1/10998.js
@@ -1,4 +1,4 @@
-const input=require("fs").readFileSync('./input.txt').toString().trim().split(' ');
+const input=require("fs").readFileSync('./input.txt', 'utf8').trim().split(' ');
 
 //A×B를 출력
 let result = input.reduce((a,b) => a*b);
